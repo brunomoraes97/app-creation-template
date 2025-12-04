fcf77ab3d8373d3e0a35286610268de2353422c8
Bruno Moraes <154920448+brunomoraes97@users.noreply.github.com>
2025-12-04 13:15:17 -0300

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,8 +9,3 @@ if [ -f package.json ]; then
   echo "[Universal Template] Installing Node dependencies..."
   npm install --silent || true
 fi
-
-if [ "$STACK" = "docker" ] || [ "$STACK" = "dockerfile" ]; then
-  echo "[Universal Template] Docker project detected — skipping npm install."
-fi
-
